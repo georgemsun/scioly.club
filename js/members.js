0bfe2256a2cbeefac6315ca7252d362f5633a6cf
George Sun <gmsun27@gmail.com>
2015-06-29 13:54:07 -0700

    Updated members.html, members.js

--- js/members.js
+++ js/members.js
@@ -2,11 +2,20 @@ $(document).ready(function() {
 	$('a').click(function() {
 		$(this).css("text-decoration", "none");
 	});
+
 	$('#calendar, #events').hide();
+	
+	var users = [
+		["interlakescienceolympiad@gmail.com", "1stplace"],
+		["gmsun@uw.edu", "bernard"],
+	];
+
 	$('#password').keyup(function() {
-		if($('#username').val().toUpperCase() == "IHS2O" && $('#password').val() == "1stplace") {
-			$('#about, #fade').fadeOut('fast');
-			$('#calendar, #events').fadeIn('fast');
+		for(var i = 0; i < users.length; i++) {
+			if($('#username').val().toUpperCase() == users[i][0].toUpperCase() && $('#password').val() == users[i][1]) {
+				$('#about, #fade').fadeOut('fast');
+				$('#calendar, #events').fadeIn('fast');
+			}
 		}
 	});
 });

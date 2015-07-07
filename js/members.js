fa8e4f5af7026d8ab88db65ea4e29c4682f550d7
George Sun <gmsun27@gmail.com>
2015-07-07 09:06:39 -0700

    Updated members.js

--- js/members.js
+++ js/members.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
 		["interlakescienceolympiad@gmail.com", "1stplace"],
 		["gmsun@uw.edu", "bernard"],
 		["magicmaster97@gmail.com", "magicalforest"],
-		["thscioly@gmail.com", "sangiotis#1"]
+		["thscioly@gmail.com", "sangiotis#1"],
+		["guest", "162452498008"]
 	];
 
 	$('#password').keyup(function() {

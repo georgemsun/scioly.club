dc1053709f69821d4c0f9d46482da54b76505e98
George Sun <gmsun27@gmail.com>
2015-07-17 22:14:44 -0700

    Updated members.html

--- js/members.js
+++ js/members.js
@@ -3,22 +3,23 @@ $(document).ready(function() {
 		$(this).css("text-decoration", "none");
 	});
 
-	$('#calendar, #events').hide();
+	$('#welcome, #calendar, #events').hide();
 	
 	var users = [
-		["interlakescienceolympiad@gmail.com", "1stplace"],
-		["gmsun@uw.edu", "bernard"],
-		["magicmaster97@gmail.com", "magicalforest"],
-		["nfliu@uw.edu", "balsa"],
-		["thscioly@gmail.com", "sangiotis#1"],
-		["guest", "162452498008"]
+		["Interlake Science Olympiad", "interlakescienceolympiad@gmail.com", "1stplace"],
+		["George", "gmsun@uw.edu", "bernard"],
+		["Edison", "magicmaster97@gmail.com", "magicalforest"],
+		["Nelson", "nfliu@uw.edu", "balsa"],
+		["Townsend Harris", "thscioly@gmail.com", "sangiotis#1"],
+		["Guest" , "guest", "162452498008"]
 	];
 
 	$('#password').keyup(function() {
 		for(var i = 0; i < users.length; i++) {
-			if($('#username').val().toUpperCase() == users[i][0].toUpperCase() && $('#password').val() == users[i][1]) {
+			if($('#username').val().toUpperCase() == users[i][1].toUpperCase() && $('#password').val() == users[i][2]) {
 				$('#about, #fade').fadeOut('fast');
-				$('#calendar, #events').fadeIn('fast');
+				$('#welcome, #calendar, #events').fadeIn('fast');
+				$('#member-name').html(users[i][0])
 			}
 		}
 	});
